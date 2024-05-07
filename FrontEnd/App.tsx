597a529fa2b070d2d02ad1af588f5ceb05a502d8
Leonardo Ravanelli <leonardo.lrc17@gmail.com>
2024-05-07 18:17:46 -0300

    tela de erro concluida

--- FrontEnd/App.tsx
+++ FrontEnd/App.tsx
@@ -7,6 +7,10 @@ const App = () => {
     return (
         <View style={styles.container}>
             <Header titulo={'Minhas Viagens'}/>
+            <View style={styles.conteudo}>
+                <Image source={require('./assets/images/telaErro/alerta.png')} style={{width: 142, height: 135}}/>
+                <Text style={styles.titulo}>Fora do Escopo</Text>
+            </View>
             <Footer />
         </View>
     );
@@ -18,6 +22,21 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
         backgroundColor: 'yellow',
+    },
+
+    conteudo: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    titulo: {
+        fontSize: 32,
+        color: 'black',
+        fontFamily: 'Outfit-VariableFont_wght',
+        fontWeight: 'bold',
+        textAlign: 'center',
     },
 });
 

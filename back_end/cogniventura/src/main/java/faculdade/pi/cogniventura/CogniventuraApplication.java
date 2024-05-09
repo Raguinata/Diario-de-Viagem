da4863c4e2cc4fbd9d179f73d18b1dbef7bcd124
RodrigoWiizz <83847745+RodrigoWiizz@users.noreply.github.com>
2024-05-08 21:52:25 -0300

    raw: configuração inicial do projeto

---
+++ back_end/cogniventura/src/main/java/faculdade/pi/cogniventura/CogniventuraApplication.java
@@ -0,0 +1,13 @@
+package faculdade.pi.cogniventura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogniventuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogniventuraApplication.class, args);
+	}
+
+}

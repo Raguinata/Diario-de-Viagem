2699687c932d006c8ffa0a69ec749cc87c9c9abc
Leonardo Ravanelli <leonardo.lrc17@gmail.com>
2024-05-09 13:09:29 -0300

    Tela Excluir Finalizada

---
+++ FrontEnd/src/pages/telaExcluir/index.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import Header from './src/components/header';
+import Footer from './src/components/footer';
+import IconVoltar from './src/components/icon-voltar';
+import { View, Text, StyleSheet, ImageBackground, Image, ScrollView, TouchableOpacity } from 'react-native';
+
+const App = () => {
+    const handleSimPress = () => {
+        console.log('Sim');
+    };
+
+    const handleNaoPress = () => {
+        console.log('Não');
+    };
+
+    return (
+        <View style={styles.container}>
+            <Header titulo={'Minhas Viagens'}/>
+            <ScrollView style={styles.conteudoScroll}>
+                <View style={styles.conteudo}>
+                    <View style={styles.iconVoltar}>
+                        <IconVoltar/>
+                    </View>
+                    
+                    <Text style={styles.titulo}>Tem certeza que deseja excluir esse item?</Text>
+
+                    <View style={styles.botoesContainer}>
+                        <TouchableOpacity style={[styles.botao, styles.botaoSim]} onPress={handleSimPress}>
+                            <Text style={styles.textoBotao}>Sim</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.botao, styles.botaoNao]} onPress={handleNaoPress}>
+                            <Text style={styles.textoBotao}>Não</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ScrollView>
+            <Footer />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#4c4c4c',
+    },
+
+    conteudoScroll: {
+        flex: 1,
+        marginVertical: 180,
+        width: '90%',
+        backgroundColor: '#D9D9D9',
+        borderRadius: 20,
+    },
+
+    conteudo: {
+        flex: 1,
+        width: '100%',
+        marginVertical: 20,
+        alignItems: 'center',
+    },
+
+    titulo: {
+        fontSize: 16,
+        color: 'black',
+        fontFamily: 'Outfit-VariableFont_wght',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginVertical: 20,
+    },
+
+    iconVoltar: {
+        width: '100%',
+    },
+
+    botoesContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '100%',
+    },
+
+    botao: {
+        borderRadius: 100,
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        minWidth: 150,
+    },
+
+    textoBotao: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+
+    botaoSim: {
+        backgroundColor: '#38b000',
+    },
+
+    botaoNao: {
+        backgroundColor: '#b00000',
+    },
+});
+
+export default App;

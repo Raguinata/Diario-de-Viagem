abfa58ce1014c480e6f075095591ffbc903b715e
RodrigoWiizz <83847745+RodrigoWiizz@users.noreply.github.com>
2024-06-07 14:12:06 -0300

    fix: endpoints de cronograma e gasto alterados

--- back_end/cogniventura/src/main/java/faculdade/pi/cogniventura/controller/GastoController.java
+++ back_end/cogniventura/src/main/java/faculdade/pi/cogniventura/controller/GastoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,7 +34,7 @@ public class GastoController {
         return ResponseEntity.ok().body(gasto);
     }
 
-    @GetMapping("/busca-por-cronograma")
+    @PostMapping("/busca-por-cronograma")
     public ResponseEntity<List<Gasto>> findByCronograma(@RequestBody Cronograma cronograma) {
         List<Gasto> gastos = gastoService.findByCronograma(cronograma);
         return ResponseEntity.ok().body(gastos);
